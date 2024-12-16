725dfb1629e7ea2fe3c3d907e57b1c859942868c
Elias Hagos <eliasars@yahoo.com>
2024-12-16 09:31:11 +0200

    Created responsive grid layout

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,33 +1,53 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks/hooks";
 import { addToCart, fetchProducts } from "../store/productSlice";
-import { Button, Container } from "react-bootstrap";
-import Product from "./Product";
-
-
+import { Button, Container, Row, Col, Card } from "react-bootstrap";
 
 const List = () => {
-    const products = useAppSelector((state) => state.products.products);
-    const dispatch = useAppDispatch();
-    console.log(products);
+  const products = useAppSelector((state) => state.products.products);
+  const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        dispatch(fetchProducts());
-      },[dispatch]);
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
-      return (
-        <div>
-            <h1>Product List</h1>
-            {products.map((product) => (
-                <Container key={product.id} lg style={{ padding: "10px"}}>
-                    <Product product={product} />
+  return (
+    <Container>
+      <h1 className="my-4 text-center">Product List</h1>
+      <Row xs={1} sm={2} md={3} className="g-4">
+        {products.map((product) => (
+          <Col key={product.id}>
+            <Card className="h-100 shadow-sm">
+              {/* Product Image */}
+              <Card.Img
+                variant="top"
+                src={product.image}
+                style={{ height: "200px", objectFit: "cover" }}
+              />
+              {/* Card Body */}
+              <Card.Body className="d-flex flex-column">
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text className="text-muted">
+                  {product.description}
+                </Card.Text>
+                <div className="mt-auto">
+                  <p className="text-success fw-bold">Price: ${product.price}</p>
+                  {/* Add to Cart Button */}
+                  <Button
+                    variant="primary"
+                    className="w-100"
+                    onClick={() => dispatch(addToCart(product))}
+                  >
+                    Add to Cart
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
 
-                    <Button onClick={() => dispatch(addToCart(product))}>
-                    Add to cart
-                    </Button>
-                </Container>
-            ))}
-        </div>
-      )
-}
 export default List;
