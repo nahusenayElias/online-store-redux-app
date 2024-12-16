ba2c4258fefbd7b9abfdf12ff0b16e115011bfa5
Elias Hagos <eliasars@yahoo.com>
2024-12-16 09:32:58 +0200

    Refactored Product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,45 +1,27 @@
-import { Card, Col, Image, Row } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 
 const Product = ({ product }) => {
-  console.log("product", product);
-
   return (
-    // <Container style={{ display: "flex", flexDirection: "row" }}>
-      <Row xs={1} md={2} lg={4} className="g-4">
-          {Array.from({ length: 1 }).map((_, idx) => (
-        <Col key={idx}>
-          <Card >
-            <Card.Img variant="top" src={product.image}className="card-img-top  img-fluid rounded"
-                style={{ height: "250px" }}/>
-            <Card.Body>
-            <Card.Title>
-
-              <h1 className="card-text">{product.name}</h1>
-            </Card.Title>
-            <Card.Text>
-
-                <p className="card-text"> {product.description}</p>
-                <div>
-                <small className="text-success text-danger">
-                  Price: ${product.price}
-                </small>
-              </div>
-            </Card.Text>
-
-
-
-
-            </Card.Body>
-          </Card>
-        </Col>
-          ))
-
-          }
-  </Row>
-  )
+    <Col xs={12} sm={6} md={4} lg={3} className="mb-4">
+      <Card>
+        <Card.Img
+          variant="top"
+          src={product.image}
+          className="card-img-top img-fluid rounded"
+          style={{ height: "250px", objectFit: "cover" }}
+        />
+        <Card.Body>
+          <Card.Title as="h5">{product.name}</Card.Title>
+          <Card.Text>{product.description}</Card.Text>
+          <Card.Text>
+            <small className="text-success">
+              Price: ${product.price}
+            </small>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
 };
+
 export default Product;
-
-
-
-
